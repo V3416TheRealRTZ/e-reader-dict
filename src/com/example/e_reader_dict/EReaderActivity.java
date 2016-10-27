a2465ed8a8ff84b40912ef96d80c2ac0678dbf62
KraftJrIvo <kraftjrivo@gmail.com>
2016-10-27 11:08:47 +0300

    books adding

--- src/com/example/e_reader_dict/EReaderActivity.java
+++ src/com/example/e_reader_dict/EReaderActivity.java
@@ -2,18 +2,23 @@ package com.example.e_reader_dict;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class EReaderActivity extends Activity {
-    LinearLayout booksScreen, learnScreen, historyScreen, topageScreen, aboutScreen;
-    ImageButton booksButton, learnButton, historyButton, topageButton, aboutButton;
-    TextView pageData;
+    private LinearLayout booksScreen, learnScreen, historyScreen, topageScreen, aboutScreen;
+    private ImageButton booksButton, learnButton, historyButton, topageButton, aboutButton;
+    private TextView pageData;
+    private FileChooser fileChooser;
+    private String programDirectory;
+    private ListView booksList;
+    private ArrayList<String> books;
+    private ArrayList<String> booksPaths;
     /**
      * Called when the activity is first created.
      */
@@ -32,6 +37,13 @@ public class EReaderActivity extends Activity {
         aboutScreen = (LinearLayout)findViewById(R.id.aboutScreen);
         aboutButton = (ImageButton) findViewById(R.id.aboutButton);
         pageData = (TextView) findViewById(R.id.pageData);
+        booksList = (ListView) findViewById(R.id.booksList);
+        books = new ArrayList<>();
+        booksPaths = new ArrayList<>();
+        createProgramDirectoryIfDoesntExist();
+        addBook(null);
+        //Log.i("File Reading stuff", programDirectory);
+        //fileChooser.
     }
 
     private void updateMenu(LinearLayout screen, ImageButton button, Boolean value) {
@@ -71,6 +83,72 @@ public class EReaderActivity extends Activity {
         return result;
     }
 
+    private void createProgramDirectoryIfDoesntExist() {
+        File directory = new File(Environment.getExternalStorageDirectory() + File.separator + "e-reader-dict");
+        directory.mkdirs();
+        programDirectory = directory.getPath();
+    }
+
+    /*private void resetBooksFile() {
+
+    }*/
+
+    private void addBook(String newBookFilePath) {
+        File booksFile = new File(programDirectory + File.separator + "books");
+        String readString = "";
+        booksPaths.clear();
+        try {
+            if (booksFile.exists()) {
+
+                FileInputStream fIn = new FileInputStream (booksFile);//openFileInput("books.txt");
+                InputStreamReader isr = new InputStreamReader(fIn);
+                BufferedReader reader = new BufferedReader(isr);
+                String line = reader.readLine();
+                do {
+                    readString += line + '\n';
+                    Log.i("Path", "path: " + line + '\n');
+                    if (!booksPaths.contains(line)) {
+                        booksPaths.add(line);
+                    }
+                    line = reader.readLine();
+                    /**/
+                } while (line != null);
+                /*
+                char[] inputBuffer = new char[10000];
+                isr.read(inputBuffer);
+                readString = new String(inputBuffer);*/
+            }
+            if (newBookFilePath != null) {
+                FileOutputStream fOut = new FileOutputStream(new File(booksFile.getPath()));//openFileOutput("books.txt", MODE_WORLD_READABLE);
+                OutputStreamWriter osw = new OutputStreamWriter(fOut);
+                osw.write(readString + newBookFilePath + '\n');
+                booksPaths.add(newBookFilePath);
+                Log.i("File Reading stuff", "read string: " + readString + newBookFilePath + '\n');
+                osw.flush();
+                osw.close();
+            }
+        } catch (IOException ioe)
+        {ioe.printStackTrace();}
+        updateBooks();
+    }
+
+    private void updateBooks() {
+        books.clear();
+        for (String bookPath : booksPaths) {
+            if (bookPath != null) {
+                String words[] = bookPath.split(File.separator);
+                String name = words[words.length - 1];
+                /*if (name.endsWith(".txt")) {
+                    name = name.substring(0, name.length() - 4);
+                } else {
+                    name = name.substring(0, name.length() - 5);
+                }*/
+                books.add(name);
+            }
+        }
+        booksList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, books));
+    }
+
     public void booksToggle(View v) {
         ArrayList<Boolean> menus = formMenusArray(5, 0, booksButton.getAlpha() != 0.5f);
         updateMenus(menus);
@@ -93,5 +171,22 @@ public class EReaderActivity extends Activity {
     public void aboutToggle(View v) {
         ArrayList<Boolean> menus = formMenusArray(5, 4, aboutButton.getAlpha() != 0.5f);
         updateMenus(menus);
+    }
+
+    public void chooseBookFile(View v) {
+        fileChooser = new FileChooser(this);
+        ArrayList<String> okExtensions = new ArrayList<>();
+        okExtensions.add(".txt");
+        okExtensions.add(".epub");
+        fileChooser.setExtensions(okExtensions);
+        fileChooser.showDialog();
+        fileChooser.setFileListener(new FileChooser.FileSelectedListener() {
+            @Override
+            public void fileSelected(File file) {
+                if (file != null) {
+                    addBook(file.getPath());
+                }
+            }
+        });
     }
 }
